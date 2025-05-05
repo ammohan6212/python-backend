c8b8253d484ab4d5228f1b121839be2149baea1d
A.Mohan <mohan6121@email.com>
2025-05-05 14:03:09 +0530

    mohan

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-# app.py
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for
+from forms import NameForm
+from flask_wtf.csrf import CSRFProtect
 
-@app.route('/')
-def hello_world():    
-    return 'Hello, World!'
+app = Flask(__name__)
+app.secret_key = 'super-secret-key'  # Needed for CSRF
+csrf = CSRFProtect(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        return f'Hello, {name}!'
+    return render_template('form.html', form=form)
 
 if __name__ == "__main__":
-    
     app.run(debug=True, host='0.0.0.0', port=5000)
